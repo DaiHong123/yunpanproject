6f3fec119f67934f9baf72b790ed57e9417852e1
Asimple <946324894@qq.com>
2018-10-13 11:44:05 +0800

    分享页面创建

---
+++ src/main/webapp/static/js/share.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	$(".asideAll").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideImg").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideText").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asidevideo").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideSeed").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideMusic").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideOther").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+	$(".asideMyShare").css({"background":"rgba(0,0,0,.05)","color":"#3b8cff"});
+	$(".asideRecycle").css({"background":"rgba(0,0,0,.01)","color":"#424e67"});
+}
