d8a18ec9a3caf7d99015ba60572dde4e00caae78
tengyijing <1273216106@qq.com>
2018-10-13 14:51:40 +0800

    文件下载

--- src/main/java/cn/qst/controller/FileController.java
+++ src/main/java/cn/qst/controller/FileController.java
@@ -9,7 +9,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import cn.qst.service.FileService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -115,6 +122,53 @@ public class FileController {
 		}
 	}
 	
+	//文件下载
+	@RequestMapping("/downlowd")
+	@ResponseBody
+    public String downlowd(String musicurl,@RequestParam(defaultValue="musicfile.mp3")String fileName,@RequestParam(defaultValue="C:\\Users\\Administrator\\Desktop")String savePath) throws IOException {
+    	URL httpUrl = new URL(musicurl);  
+		HttpURLConnection conn = (HttpURLConnection)httpUrl.openConnection();  
+                //设置超时间为3秒
+		conn.setConnectTimeout(3*1000);
+		//防止屏蔽程序抓取而返回403错误
+		conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+ 
+		//得到输入流
+		InputStream inputStream = conn.getInputStream();  
+		//获取自己数组
+		byte[] getData = readInputStream(inputStream);    
+ 
+		//文件保存位置
+		File saveDir = new File(savePath);
+		if(!saveDir.exists()){
+			saveDir.mkdir();
+		}
+		File file = new File(saveDir+File.separator+fileName);    
+		FileOutputStream fos = new FileOutputStream(file);     
+		fos.write(getData); 
+		if(fos!=null){
+			fos.close();  
+		}
+		if(inputStream!=null){
+			inputStream.close();
+		}
+		if(file!=null) {
+			return "200";
+		}
+		return "500";
+    }
+	
+	//读取文件
+	private  byte[] readInputStream(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while((len = inputStream.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+        bos.close();
+        return bos.toByteArray();
+    }
 	
 	//重命名文件
 	@RequestMapping("/rename")
